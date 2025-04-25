fcf01bf7140b5fc48aa0dabc00df4f8f50e4a34a
taha7486 <tahaessamit@gmail.com>
2025-04-25 10:37:43 +0100

    SCRUM-18 WORKING

--- mysql/docker-entrypoint-initdb.d/02-data.sql
+++ mysql/docker-entrypoint-initdb.d/02-data.sql
@@ -5,8 +5,3 @@ INSERT IGNORE INTO room_types (name, base_price, description) VALUES
   ('Deluxe', 200.00, '1 king bed, premium view'),
   ('Suite', 350.00, 'Separate living area, jacuzzi');
 
--- Amenities  
-INSERT IGNORE INTO amenities (name, icon) VALUES
-  ('WiFi', 'wifi'),
-  ('Pool', 'pool'),
-  ('Gym', 'fitness_center');
